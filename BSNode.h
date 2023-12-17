60102ded5b58512de1a69f24cf986526305e4336
MAMAALF <mamaalf@alumno.upv.es@ldsic-vdi02.upvnet.upv.es>
2023-12-17 11:42:37 +0100

    Añadida implementación de la clase BSNode

--- BSNode.h
+++ BSNode.h
@@ -9,15 +9,15 @@ class BSNode {
         // miembros públicos
 
         T elem;      // El elemento de tipo T almacenado en el nodo.
-        BSNode<T>* left;     // Puntero al nodo sucesor izquierdo.
-        BSNode<T>* right;    // Puntero al nodo sucesor derecho.
+        BSNode<T>* izq;     // Puntero al nodo sucesor izquierdo.
+        BSNode<T>* der;    // Puntero al nodo sucesor derecho.
 
 
         // Método constructor que crea un BSNode con el elemento elem y los punteros a los nodos sucesores left y  right proporcionados (nulos por defecto). 
-        BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr){
+        BSNode(T elem, BSNode<T>* izq=nullptr, BSNode<T>* der=nullptr){
             this->elem = elem;
-            this->left = left;
-            this->right = right;
+            this->izq = izq;
+            this->der = der;
         }
 
         // Sobrecarga global del operador << para imprimir el nodo por pantalla. Por simplicidad, puedes limitarte a imprimir el atributo elem. Recuerda incluir la cabecera <ostream> en el .h.
